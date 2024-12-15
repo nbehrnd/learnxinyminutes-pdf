bc7c456fbe943214b2d558aeae962d0a48270077
Norwid Behrnd <nbehrnd@yahoo.com>
2024-12-15 19:02:00 +0100

    style(upload_relases.py): reformat with black
    For consistency of style, black (version 24.10.0) reformats the script.
    
    Signed-off-by: Norwid Behrnd <nbehrnd@yahoo.com>

--- upload_releases.py
+++ upload_releases.py
@@ -7,58 +7,59 @@ import shutil
 
 release = []
 
-def upload(file, mime = 'application/pdf'):
-	name = os.path.basename(file)
-	fdata = open(file, 'rb').read()
-	asset = release.upload_asset(mime, name, fdata)
-	return asset
+
+def upload(file, mime="application/pdf"):
+    name = os.path.basename(file)
+    fdata = open(file, "rb").read()
+    asset = release.upload_asset(mime, name, fdata)
+    return asset
 
 
-username = 'aviaryan'
-repo = 'learnxinyminutes-pdf'
-tokentxt = open('token.txt', 'r').read()
-gh = login(token = tokentxt)
+username = "aviaryan"
+repo = "learnxinyminutes-pdf"
+tokentxt = open("token.txt", "r").read()
+gh = login(token=tokentxt)
 
 user = gh.user(username)
 repo = gh.repository(username, repo)
 print(user.name)
 
 for i in repo.releases():
-	print(i)
-	release = i
-	print(i.tag_name, 'name = ', i.name, i.body, i.id)
-	break
+    print(i)
+    release = i
+    print(i.tag_name, "name = ", i.name, i.body, i.id)
+    break
 
 # upload main pdf
-file = 'learnxinyminutes.pdf'
-print('Uploading ' + file)
+file = "learnxinyminutes.pdf"
+print("Uploading " + file)
 try:
-	upload(file)
+    upload(file)
 except Exception as e:
-	print(file, 'upload failed %s' % str(e))
-	exit(1)
+    print(file, "upload failed %s" % str(e))
+    exit(1)
 
 count = 0
 # upload single pdf's
-for file in os.listdir('_pdfs'):
-	print('Uploading ' + file)
-	try:
-		upload('_pdfs/' + file)
-		count += 1
-	except:
-		print('Failed', file)
-print('Uploaded', count, 'single pdfs')
+for file in os.listdir("_pdfs"):
+    print("Uploading " + file)
+    try:
+        upload("_pdfs/" + file)
+        count += 1
+    except:
+        print("Failed", file)
+print("Uploaded", count, "single pdfs")
 
 # upload all pdf
-zipname = 'learnxinyminutes_all'
-print('Uploading ' + zipname)
-if os.path.isfile(zipname + '.zip'):
-	os.remove(zipname + '.zip')
-shutil.make_archive(zipname, 'zip', '_pdfs') # the all pdf
+zipname = "learnxinyminutes_all"
+print("Uploading " + zipname)
+if os.path.isfile(zipname + ".zip"):
+    os.remove(zipname + ".zip")
+shutil.make_archive(zipname, "zip", "_pdfs")  # the all pdf
 try:
-	upload(zipname + '.zip', 'application/zip')
+    upload(zipname + ".zip", "application/zip")
 except:
-	print('all zip failed')
+    print("all zip failed")
 
 # release = repo.create_release(version)
 # print(release.edit(body='test release by API'))
